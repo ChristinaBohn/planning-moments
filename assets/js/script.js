5eb2cae91168697d1fba2167d792130402497380
Christina Bohn <christinajbohn@gmail.com>
2021-12-07 12:37:25 -0800

    Began funtion for changing color of rows to match current hour.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,32 +21,37 @@ scheduleTableEl.on('click', handleFormSubmit);
 
 console.log(dailyTaskList);
 
+// Save input text and print to page
+
+
+// Change color of rows depending on current hour
+
+var currentHour = moment().format("h");
+var allHourOptions = "array of all hour rows"
+
+for (i = 0; i < hourOptions.length; i++) {
+    var hourRow = allHourOptions[i];
+
+    if (hourRow === currentHour) {
+
+        // set class to .present
+
+    } else if (hourRow > currentHour) {
+
+        // set class to .future
+
+    } else {
+
+        // set class to .past
+
+    }
+
+}
 
 
 
 
-// var shoppingFormEl = $('#shopping-form');
-// var shoppingListEl = $('#shopping-list');
 
-// // create function to handle form submission
-// function handleFormSubmit(event) {
-//   event.preventDefault();
 
-//   // select form element by its `name` attribute and get its value
-//   var shoppingItem = $('input[name="shopping-input"]').val();
 
-//   // if there's nothing in the form entered, don't print to the page
-//   if (!shoppingItem) {
-//     console.log('No shopping item filled out in form!');
-//     return;
-//   }
 
-//   // print to the page
-//   shoppingListEl.append('<li>' + shoppingItem + '</li>');
-
-//   // clear the form input element
-//   $('input[name="shopping-input"]').val('');
-// }
-
-// // Create a submit event listener on the form element
-// shoppingFormEl.on('submit', handleFormSubmit);
